356060dc699bb3a9faf699623bf3c212fa58dd53
Hatice Taylan <htc_tylnn@hotmail.com>
2026-05-10 13:30:40 +0200

    finished get method inclusive the tests

---
+++ src/main/java/com/hati/cashcard/CashCardController.java
@@ -0,0 +1,21 @@
+package com.hati.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // handle http requests
+@RequestMapping("/cashcards") // which addresses this controller will handle
+class CashCardController {
+
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if(requestedId != 99L) {
+            return ResponseEntity.notFound().build();
+        }   
+        CashCard cashCard = new CashCard(99L, 123.45);
+        return ResponseEntity.ok(cashCard);
+    }
+}
--- src/test/java/com/hati/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/hati/cashcard/CashcardApplicationTests.java
@@ -19,7 +19,14 @@ class CashCardApplicationTests {
     @Test
     void shouldReturnACashCardWhenDataIsSaved() {
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
-
+        //check the status code
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        //check the id
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(99);
+        //check the amount
+        Double amount = documentContext.read("$.amount");
+        assertThat(amount).isEqualTo(123.45);
     }
 }
