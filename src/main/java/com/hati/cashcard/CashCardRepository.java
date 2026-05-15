2c68bf1093ab39584cb0d593d5c748c8a2e94fbc
Hatice Taylan <htc_tylnn@hotmail.com>
2026-05-15 11:26:32 +0200

    added comments to the code

--- src/main/java/com/hati/cashcard/CashCardRepository.java
+++ src/main/java/com/hati/cashcard/CashCardRepository.java
@@ -2,5 +2,7 @@ package com.hati.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
 
+//repository interface, extends CrudRepository to get basic CRUD operations for free, no need to implement it
+// such as save(), findById(), findAll(), deleteById() etc.
 interface CashCardRepository extends CrudRepository<CashCard, Long> {
 }
