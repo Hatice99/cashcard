85c13ae716849bc4dd693ba8f1be36bfb70c1d81
Hatice Taylan <htc_tylnn@hotmail.com>
2026-05-16 13:46:46 +0200

    added basic authentication

--- src/test/java/com/hati/cashcard/CashCardJsonTest.java
+++ src/test/java/com/hati/cashcard/CashCardJsonTest.java
@@ -25,9 +25,9 @@ class CashCardJsonTest {
     @BeforeEach
     void setUp() {
         cashCards = new CashCard[] {
-                new CashCard(99L, 123.45),
-                new CashCard(100L, 1.00),
-                new CashCard(101L, 150.00)
+                new CashCard(99L, 123.45, "sarah1"),
+                new CashCard(100L, 1.00, "sarah1"),
+                new CashCard(101L, 150.00, "sarah1")
         };
     }
 
@@ -42,9 +42,9 @@ class CashCardJsonTest {
     void cashCardListDeserializationTest() throws IOException {
         String expected = """
                 [
-                   { "id": 99, "amount": 123.45 },
-                   { "id": 100, "amount": 1.00 },
-                   { "id": 101, "amount": 150.00 }
+                   { "id": 99, "amount": 123.45, "owner": "sarah1" },
+                   { "id": 100, "amount": 1.00, "owner": "sarah1" },
+                   { "id": 101, "amount": 150.00, "owner": "sarah1" }
                 ]
                 """;
         assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
