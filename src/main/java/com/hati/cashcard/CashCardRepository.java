30bb3e7b4fc337e84713eddb14e61d0bf30c3cd0
Hatice Taylan <htc_tylnn@hotmail.com>
2026-05-16 11:43:52 +0200

    added pagination with sorting

--- src/main/java/com/hati/cashcard/CashCardRepository.java
+++ src/main/java/com/hati/cashcard/CashCardRepository.java
@@ -1,8 +1,9 @@
 package com.hati.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 //repository interface, extends CrudRepository to get basic CRUD operations for free, no need to implement it
 // such as save(), findById(), findAll(), deleteById() etc.
-interface CashCardRepository extends CrudRepository<CashCard, Long> {
+interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
